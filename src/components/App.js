e9ae478fbec40663ac29968529b1337f84790a0d
Massimiliano Marini <massimiliano.marini@gmail.com>
2017-02-06 18:27:17 +0100

    Added the LICENSE file

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div clanssName="App">
+        Welcome to the React world!
+      </div>
+    );
+  }
+}
+
+export default App;
